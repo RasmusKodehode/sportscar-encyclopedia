560474f83e8aa905a6639f95319674a2509a8218
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-15 20:56:07 +0200

    reduced py

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full flex-col items-center justify-start py-32 px-16 bg-white sm:items-start gap-5">
+      <main className="flex flex-1 w-full flex-col items-center justify-start py-12 px-16 bg-white sm:items-start gap-5">
         <div className="flex flex-col gap-2">
           <h1 className="text-4xl font-extrabold">
             Welcome to the sportscar encyclopedia!
