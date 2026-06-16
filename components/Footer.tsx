2badc999f79048e035cf0e7f36c70a8e652ce784
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-16 15:06:03 +0200

    fixed styling footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="flex flex-row justify-between w-full h-40 px-6 py-7 bg-blue-950 text-white">
-      <div className="flex flex-col items-center w-1/3">
+      <div className="flex flex-col items-baseline justify-between w-1/3">
         <p>Sportscar encyclopedia</p>
         <p>2022 - present</p>
       </div>
-      <div className="flex flex-col justify-center items-end w-1/3">
+      <div className="flex flex-col justify-end items-center w-1/3">
         <p>Copyright 2026</p>
       </div>
       <div className="flex flex-col justify-baseline w-1/3">
