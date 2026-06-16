97ca47ec27f74c29d14763878db817d12f693137
RasmusKodehode <rasmus.kodehode@gmail.com>
2026-06-16 14:58:06 +0200

    fixed column widths

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="flex flex-row justify-between w-full h-40 px-6 py-7 bg-blue-950 text-white">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center w-1/3">
         <p>Sportscar encyclopedia</p>
         <p>2022 - present</p>
       </div>
-      <div className="flex flex-col justify-center items-end">
+      <div className="flex flex-col justify-center items-end w-1/3">
         <p>Copyright 2026</p>
       </div>
-      <div className="flex flex-col justify-baseline">
+      <div className="flex flex-col justify-baseline w-1/3">
         <p>
           Thanks to FIA, ACO and IMSA for organizing great championships and
           thanks to the manufacturers for bringing great cars
